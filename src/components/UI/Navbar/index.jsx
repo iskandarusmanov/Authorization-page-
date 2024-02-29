a015e2767ea009d5dcfc440fa5c366a00b8de2e2
iskandarusmanov <usman29104@gmail.com>
2024-02-29 16:55:11 +0500

    fix

--- src/components/UI/Navbar/index.jsx
+++ src/components/UI/Navbar/index.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
         <button
         onClick={() => {
           localStorage.clear();
-          // navigate("/login")
+          navigate(0)
         }}
         className="nav__btn"
         >
